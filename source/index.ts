028151f93946eb0067cb8c5dc96b1b57c2178b4a
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-01 03:15:51 +0500

    Udpate

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,44 @@
-let n: number = 234
+import inquirer from "inquirer";
+// Start of Application
+Application()
+
+
+
+//Operation of Full Application 
+async function Application() {
+    const answers = await inquirer.prompt([
+        
+        {
+            type: "input",
+            name: "inputCash",
+            message: "Select Your Currency",
+        },
+        {
+            type: "list",
+            name: "inputCurrency",
+            message: "Select Your Currency",
+            choices: [
+                "PKR",
+                "INR",
+                "USD",
+                "PND",
+                "EUR",
+            ]
+        },
+        {
+            type: "list",
+            name: "inputCurrency",
+            message: "Select Your Currency",
+            choices: [
+                "PKR",
+                "INR",
+                "USD",
+                "PND",
+                "EUR",
+            ]
+        }
+        
+
+    ])
+
+}
