73c89a33d7472b676da8d99b0d8a616772e2cac9
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-02 03:40:28 +0500

    NPX Udpate

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-
 import inquirer from "inquirer";
 // Start of Application
 //Operation of Full Application
 async function Application() {
     const answers = await inquirer.prompt([
-
         {
             type: "input",
             name: "inputCash",
@@ -29,41 +27,36 @@ async function Application() {
                 "USD",
             ]
         }
-
-
-    ])
+    ]);
     if (answers.inputCash.toLocaleLowerCase() === answers.outputCurrency.toLocaleLowerCase()) {
-        console.log('Conversation in same currency is not logical')
-        Application()
+        console.log('Conversation in same currency is not logical');
+        Application();
     }
     else {
-        CurrencyConveter(answers.inputCurrency, answers.outputCurrency, answers.inputCash)
+        CurrencyConveter(answers.inputCurrency, answers.outputCurrency, answers.inputCash);
         const restart = await inquirer.prompt([
             {
                 type: "input",
                 message: "You want to convert More ! Yes/NO ",
                 name: "check"
             }
-        ])
+        ]);
         if (restart.check.toLocaleLowerCase() === 'yes') {
-            Application()
+            Application();
         }
         else {
-            console.log('Thanks For Using Our Program Stay Happy and Blessed')
+            console.log('Thanks For Using Our Program Stay Happy and Blessed');
         }
     }
-
 }
-async function CurrencyConveter(input: string, output: string, cash: number) {
+async function CurrencyConveter(input, output, cash) {
     if (input === "USD" && output === "PKR") {
-        console.log(`* Fresh Rate Accourding to Open Market - 1 USD = 280 PKR`)
-        console.log(` You have ${cash} $ USD which equal to ${Number(cash * 280)} PKR`)
+        console.log(`* Fresh Rate Accourding to Open Market - 1 USD = 280 PKR`);
+        console.log(` You have ${cash} $ USD which equal to ${Number(cash * 280)} PKR`);
     }
     else if (input === "PKR" && output === "USD") {
-        console.log(`* Fresh Rate Accourding to Open Market - 1 USD = 280 PKR`)
-        console.log(` You have ${cash} PKR which equal to ${Number(cash / 280)} USD`)
+        console.log(`* Fresh Rate Accourding to Open Market - 1 USD = 280 PKR`);
+        console.log(` You have ${cash} PKR which equal to ${Number(cash / 280)} USD`);
     }
-
 }
-
-Application()
+Application();
